d1ea1072c1e3cae852c3497a913dc8e343fe5fa7
Otabek <otabek.baratov@ilogos.biz>
2023-09-26 22:48:15 +0500

    initial commit
    plugins added

---
+++ Assets/Scripts/Core/Infrastructure/GameLifetimeScope.cs
@@ -0,0 +1,13 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+namespace Core.Infrastructure
+{
+    public class GameLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            
+        }
+    }
+}
